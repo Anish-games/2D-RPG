e92a16771b969b3492c28ab7752f76d0a3a4505b
MiniNija <anishkute12@gmail.com>
2025-06-15 02:20:40 +0530

    added player & gun

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using NUnit;
+using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float enemyHealth;
+    public GameObject deadEffect;
+
+
+
+    public void takeDamage(int damage)
+    {
+        enemyHealth -= damage;
+
+        if (enemyHealth <= 0)
+        {
+            die();
+        }
+    }
+
+    private void die()
+    {
+        Instantiate(deadEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
+
+
